6116813a59b874c416cfd6ded355e711293abf34
Manmohan <50476746+manmohanganesh@users.noreply.github.com>
2020-09-20 20:03:41 +0530

    Add files via upload
    To scrap amazon web data

---
+++ bsscrap.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+headers = {
+    'authority': 'www.amazon.com',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'none',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-dest': 'document',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+}
+urls = ['https://www.amazon.in/s?k=mobile&rh=n%3A1389401031&dc&ref=a9_sc_1',
+        'https://www.amazon.in/s?k=laptops&ref=nb_sb_noss_2',
+        'https://www.amazon.in/s?k=headphones&ref=nb_sb_noss_2']
+
+def scrapping(link):
+    driver = webdriver.Chrome("/usr/bin/chromedriver")
+    driver.get(link)
+
+    page_soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    title = page_soup.find_all("span", {'class': 'a-size-medium a-color-base a-text-normal'})
+    price = page_soup.find_all("span", {'class': 'a-price-whole'})
+
+    count = len(title)
+
+    try:
+        for i in range(count):
+            print(title[i].text)
+            print('Rs.', price[i].text)
+            print("\n")
+
+    except:
+        print('Parsing error')
+
+
+x = len(urls)
+for j in range(x):
+    print("\n\n", (j+1), 'Category')
+    scrapping(urls[j])
+
+
